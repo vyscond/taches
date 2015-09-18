4cec1b4ca42f52040d45c9c73e395bb3892f7bf5
vyscond <vyscond@gmail.com>
2015-09-18 00:39:58 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+setup(
+    name = "taches",
+    author = "vyscond",
+    author_email = "vyscond@gmail.com",
+    version = "0.0.0",
+    description = "minimal python based task runner",
+    long_description = "minimal python based task runner",
+    url = "https://www.github.com/vyscond/tache",
+    license = "MIT",
+    extras_require = {
+        "dev": [
+            "nest"
+        ]
+    }
+)
